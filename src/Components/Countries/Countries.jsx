c3e14397cdf126331e8da05925b447d625831edd
Mahadi176 <mahadi210176@gmail.com>
2026-03-28 15:09:19 +0600

    push

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,16 +1,32 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
+    const [visitedCountries,setVisitedCountries] = useState([])
+
+    const handleVisitedCountries = (country) =>
+      {
+       console.log("visited",country)
+       const newVisitedCountry = [...visitedCountries,country]
+       setVisitedCountries(newVisitedCountry)
+    }
+
     const countriesData = use(countriesPromise)
     const countries = countriesData.countries
     return (
-        <div>
+      <div>
             <h1>In The Countries : {countries.length}</h1>
+            <p>Total Country Visited : {visitedCountries.length}</p>
+            <ol>
+            {  visitedCountries.map(country => <li>{country.name.common}</li> ) }
+            </ol>
+        <div className='Countries'>
           {
-            countries.map(country => <Country country={country}></Country>)
+            countries.map(country => <Country key={country.cca3.cca3} country={country} handleVisitedCountries={handleVisitedCountries}></Country>)
           }
         </div>
+      </div>
     );
 };
 
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css';
+const Country = ({country,handleVisitedCountries}) => {
+     
+    const [visited,SetVisited] = useState(false)
 
-const Country = ({country}) => {
-    console.log(country.name.common)
+    const handleVisited = () =>{
+            SetVisited(true)
+            handleVisitedCountries(country)
+    }
+
     return (
-        <div>
+        <div className='Country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-            <p>Name : {country.name.common}</p>
+            <h4>Name : {country.name.common}</h4>
+            <p>Population :{country.population.population} </p>
+            <p>Area : {country.area.area} sqKm {country.area.area > 300000 ? '(Big Country)': '(Small Country)'}</p>
+
+            <button onClick={handleVisited}>
+                {visited ? 'Visited':'Not Visited'}
+            </button>
         </div>
     );
 };
